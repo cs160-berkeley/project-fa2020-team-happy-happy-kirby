ab76196d1898aea484059fa354a4757c6e93307b
Nicholas Saquilayan <nsaquilayan@berkeley.edu>
2020-11-17 20:28:27 -0800

    Create AccessibilityAdapter and update accesibility layout

---
+++ app/src/main/java/com/gmail/kingarthuralagao/us/civicengagement/AccessibilityAdapter.java
@@ -0,0 +1,12 @@
+package com.gmail.kingarthuralagao.us.civicengagement;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AccessibilityAdapter extends BaseAdapter {
+    @Override
+    public View getView(int position, View convertView, ViewGroup container) {
+
+    }
+}
