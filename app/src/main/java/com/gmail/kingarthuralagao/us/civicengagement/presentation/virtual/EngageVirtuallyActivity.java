6b34aa1aa304e63d5a98696c900ea5b1f95b7082
King Arthur Alagao <kialagao@berkeley.edu>
2020-12-01 23:57:14 -0800

    Idk

--- app/src/main/java/com/gmail/kingarthuralagao/us/civicengagement/presentation/virtual/EngageVirtuallyActivity.java
+++ app/src/main/java/com/gmail/kingarthuralagao/us/civicengagement/presentation/virtual/EngageVirtuallyActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.gmail.kingarthuralagao.us.civilengagement.databinding.ActivityEngageVirtuallyBinding;
 
 import org.jsoup.Jsoup;
-import org.w3c.dom.Document;
+import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
@@ -24,26 +24,23 @@ public class EngageVirtuallyActivity extends AppCompatActivity {
 
         // Get the amount raised and goal amount from the Go Fund Me page.
         String url = "https://www.gofundme.com/f/memorial-expenses-for-sig-and-helen-decker";
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Document document = null;
-                try {
-                    document = Jsoup.connect(url).get();
-                    String fullString = document.select(".m-progress-meter-heading").text();
-                    System.out.println(fullString);
-                    String[] split = fullString.split(" ");
-                    String raised = split[0];
-                    String goal = split[3];
-                    Log.d("EngageVirtually", "Amount raised: " + raised);
-                    Log.d("EngageVirtually", "Amount needed: " + goal);
-                } catch (IOException e) {
-                    Log.d("EngageVirtually", e.getMessage());
-                    e.printStackTrace();
-                }
-
+        Thread thread = new Thread(() -> {
+            Document document = null;
+            try {
+                document = Jsoup.connect(url).get();
+                String fullString = document.select(".m-progress-meter-heading").text();
+                System.out.println(fullString);
+                String[] split = fullString.split(" ");
+                String raised = split[0];
+                String goal = split[3];
+                Log.d("EngageVirtually", "Amount raised: " + raised);
+                Log.d("EngageVirtually", "Amount needed: " + goal);
+            } catch (IOException e) {
+                Log.d("EngageVirtually", e.getMessage());
+                e.printStackTrace();
             }
-        }).start();
+
+        });
 
         thread.start();
         binding.progressIdc.setProgress(50);
